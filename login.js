ecdd46ee9282a956ff357592d86b5038c5c48565
Yasharth Singh <143637324+yasharth-0910@users.noreply.github.com>
2024-01-20 16:05:48 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Script loaded!");
+
+  var loginForm = document.getElementById("loginForm");
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Get the entered username and password
+    var enteredUsername = document.getElementById("username").value;
+    var enteredPassword = document.getElementById("password").value;
+
+    // Check if the username and password are correct
+    if (enteredUsername === "yasharth" && enteredPassword === "yasharth"||enteredUsername === "yash" && enteredPassword === "ninni"||enteredUsername === "divyansh" && enteredPassword === "topper"||enteredUsername === "ali" && enteredPassword === "alibhaii"||enteredUsername === "himamnshu" && enteredPassword === "cinekala_ka_dalal"||enteredUsername === "rakshit" && enteredPassword === "pookiebear") {
+      // Redirect to the homepage
+      window.location.href = "welcome.html";
+    } else {
+      // Show an error message
+      alert("Invalid username or password. Please try again.");
+    }
+  });
+});
---
+++ welcome.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var welcomeText = document.getElementById("welcomeText");
+    var cursor = document.getElementById("cursor");
+    var textToType = "Welcome to CCU";
+    var currentIndex = 0;
+
+    function typeText() {
+        if (currentIndex <= textToType.length) {
+            welcomeText.innerHTML = textToType.substring(0, currentIndex);
+            cursor.style.display = "inline"; 
+            currentIndex++;
+            setTimeout(typeText, 150); // timing
+        } else {
+            setTimeout(eraseText, 1000);
+        }
+    }
+
+    function eraseText() {
+        var currentText = welcomeText.innerHTML;
+        if (currentText.length > 0) {
+            welcomeText.innerHTML = currentText.slice(0, -1);
+            cursor.style.display = "inline"; 
+            setTimeout(eraseText, 120); // timing
+            currentIndex = 0;
+            setTimeout(typeText, 500);
+        }
+    }
+
+    typeText();
+
+    var nextButton = document.getElementById("nextButton");
+    nextButton.addEventListener("click", function () {
+        redirectToHomepage();
+    });
+
+    function redirectToHomepage() {
+        window.location.href = "homepage.html";
+    }
+});
